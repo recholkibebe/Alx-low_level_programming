127f6aadf3f8294e7f19b8307414e5519fdbc714
‘recholkibebe’ <recholkibebe@gmail.com>
2023-10-09 13:02:08 -0700

    size six

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main -entry point to program.
- * Description -A program that prints the  size of various types on the computer it is compiled and run on.
+ * Description -A program that prints the  size.
  * Return: 0 always to dindicate success.
  */
 int main(void)
