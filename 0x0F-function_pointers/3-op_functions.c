7482a4e1b60a3960ea626f413dea288f9b03c947
‘recholkibebe’ <recholkibebe@gmail.com>
2023-11-08 13:35:37 -0800

    3get

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,66 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * op_add - prints sum of a and b
+ * @a:first integer
+ * @b:second integer
+ * Return: sum
+ */
 
-	int op_add(int a, int b);
-	int op_sub(int a, int b);
-	int op_mul(int a, int b);
-	int op_div(int a, int b);
-	int op_mod(int a, int b);
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
+/**
+ * op_sub - multiplies numbers
+ * @a:first integer
+ * @b:second integer
+ * Return: difference
+ */
 
-	/**
-	 * op_add - Returns the sum of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The sum of a and b.
-	 */
-	int op_add(int a, int b)
-	{
-		return (a + b);
-	}
-	/**
-	 * op_sub - Returns the difference of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The difference of a and b.
-	 */
-	int op_sub(int a, int b)
-	{
-		return (a - b);
-	}
-	/**
-	 * op_mul - Returns the product of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The product of a and b.
-	 */
-	int op_mul(int a, int b)
-	{
-		return (a * b);
-	}
-	/**
-	 * op_div - Returns the division of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The quotient of a and b.
-	 */
-	int op_div(int a, int b)
-	{
-		return (a / b);
-	}
-	/**
-	 * op_mod - Returns the remainder of the division of two numbers.
-	 * @a: The first number.
-	 * @b: The second number.
-	 *
-	 * Return: The remainder of the division of a by b.
-	 */
-	int op_mod(int a, int b)
-	{
-		return (a % b);
-	}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
+/**
+ * op_mul - prints product
+ * @a:first integer
+ * @b:second integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a:first int
+ * @b:second int
+ * Return: result on success
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod -prints remainder
+ * @a:first int
+ * @b:second int
+ * Return: remainder on success
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
