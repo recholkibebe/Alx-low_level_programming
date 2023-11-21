edfb34935a2570919acea553f4453f303b3d4123
‘recholkibebe’ <recholkibebe@gmail.com>
2023-11-21 15:14:43 -0800

    The free file

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,37 +1,40 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linekd list
- * @h:poi102-free_listint_safe.c nter to first node in list
- * Return: number of elemnts freed
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
-
 size_t free_listint_safe(listint_t **h)
 {
-	size_t length = 0;
-	int tof;
+	size_t len = 0;
+	int diff;
 	listint_t *temp;
 
 	if (!h || !*h)
 		return (0);
+
 	while (*h)
 	{
-		tof = *h - (*h)->next;
-		if (tof > 0)
+		diff = *h - (*h)->next;
+		if (diff > 0)
 		{
 			temp = (*h)->next;
 			free(*h);
 			*h = temp;
-			length++;
+			len++;
 		}
 		else
 		{
 			free(*h);
 			*h = NULL;
-			length++;
+			len++;
 			break;
 		}
 	}
+
 	*h = NULL;
-	return (length);
+
+	return (len);
 }
